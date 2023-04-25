c0d372d23787e8029510ffcc061a6fc5087bd6e1
Victor Ha <victorha88@gmail.com>
2023-04-25 14:08:44 -0400

    Implement User Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  
+
   protect_from_forgery with: :exception
 
   private
@@ -9,24 +9,36 @@ class ApplicationController < ActionController::Base
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
+
   helper_method :cart
 
   def enhanced_cart
-    @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
+    @enhanced_cart ||= Product.where(id: cart.keys).map { |product| { product: product, quantity: cart[product.id.to_s] } }
   end
+
   helper_method :enhanced_cart
 
   def cart_subtotal_cents
-    enhanced_cart.map {|entry| entry[:product].price_cents * entry[:quantity]}.sum
+    enhanced_cart.map { |entry| entry[:product].price_cents * entry[:quantity] }.sum
   end
-  helper_method :cart_subtotal_cents
 
+  helper_method :cart_subtotal_cents
 
   def update_cart(new_cart)
     cookies[:cart] = {
       value: JSON.generate(new_cart),
-      expires: 10.days.from_now
+      expires: 10.days.from_now,
     }
     cookies[:cart]
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def authorize
+    redirect_to "/login" unless current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,35 @@
 Rails.application.routes.draw do
-
   namespace :admin do
-    get 'categories/index'
+    get "categories/index"
   end
 
-  get 'about/index'  
-  root to: 'products#index'
+  get "about/index"
+  root to: "products#index"
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
-    post   :add_item
-    post   :remove_item
+    post :add_item
+    post :remove_item
   end
 
-  get '/about', to: 'about#index'
+  get "/about", to: "about#index"
 
   resources :orders, only: [:create, :show]
 
   namespace :admin do
-    root to: 'dashboard#show'
+    root to: "dashboard#show"
     resources :products, except: [:edit, :update, :show]
     resources :categories, except: [:edit, :update, :show]
   end
+
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  get "/logout" => "sessions#destroy"
+
+  get "/signup" => "users#new"
+  post "/users" => "usres#create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
