e930a848b52e22d3eff4086fbc46302a8f4071b5
Victor Ha <victorha88@gmail.com>
2023-04-24 22:37:08 -0400

    Implement Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :admin do
+    get 'categories/index'
+  end
+
   get 'about/index'  
   root to: 'products#index'
 
@@ -18,6 +22,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
